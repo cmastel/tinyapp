9dcffd0ffe71a64ed36daa94a741fec6173bb090
Chris Mastel <chris.mastel@gmail.com>
2019-10-30 00:05:23 +0000

    added comments throughout

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// load required modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -9,13 +10,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
+// initialize starting "database"
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://google.com"
 };
 
-
 function generateRandomString() {
+  // generates a random alpha-numeric string of 6 characters
   let randomString = '';
   const r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   for (let i = 0; i < 6; i++) {
@@ -29,6 +31,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  // primary urls page, with summary of shortURL's and the
+  // associated longURL's
+  // each url pair can be Edited or Deleted from here
   let templateVars = { 
     username: req.cookies.username,
     urls: urlDatabase 
@@ -37,6 +42,8 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  // /urls/new is a page where the user can specify and full URL,
+  // so that the app can create a shortURL to associate with it
   let templateVars = { 
     username: req.cookies.username,
   };
@@ -44,45 +51,57 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  // shows a page where a shortURL and longURL pair are summarized
+  // a user can then edit the longURL associated with the shortURL
   let templateVars = { 
     username: req.cookies.username,
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL] 
   };
-  res.render("urls_show", templateVars);
+  res.render("/urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
+  // redirects the user to the website using the longURL associated
+  // with a given shortURL
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 })
 
 app.post("/urls", (req, res) => {
+  // creates a randome shortURL for a user inputed longURL
+  // adds the new shortURL and longURL pair to the "database"
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
+  // removes a shortURL and longURL pair from the "database"
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
+  // allows the user to set a new longURL for a given shortURL
   urlDatabase[req.params.shortURL] = req.body.newLongURL;
   res.redirect("/urls");
 });
 
 app.post("/login", (req, res) => {
+  // when a user logs in, sets a cookie to store their username
   res.cookie('username', req.body.username);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
+  // when a user selects to logout, removes their username from 
+  // the cookie
   res.clearCookie('username');
   res.redirect("/urls");
 });
 
+// set up a server to listen on the specified port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
