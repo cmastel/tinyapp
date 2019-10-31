cf6dccaf6df6260b22e7e3c035a103d02141c0b2
Chris Mastel <chris.mastel@gmail.com>
2019-10-31 15:58:47 +0000

    deleted commented-out helper functions from express_server.js

--- express_server.js
+++ express_server.js
@@ -39,44 +39,6 @@ const users = {
   }
 };
 
-// function generateRandomString() {
-//   // generates a random alpha-numeric string of 6 characters
-//   let randomString = '';
-//   const r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-//   for (let i = 0; i < 6; i++) {
-//     randomString += r[Math.floor(Math.random() * r.length)];
-//   };
-//   return randomString;
-// };
-
-// function getUserID(newEmail, database) {
-//   // checks if a provided email address is already in the users "database"
-//   let userID = false;
-//   for (let user in database) {
-//     if (database[user].email === newEmail) {
-//       userID = user;
-//     }; 
-//   };
-//   return userID;
-// };
-
-// function urlsForUser(id) {
-//   // iterates through the urlDatase object
-//   let urlsFiltered = {};
-//   for (let shortURL in urlDatabase) {
-//     if (urlDatabase[shortURL].userID === id) {
-//       urlsFiltered[shortURL] = urlDatabase[shortURL].longURL;
-//     };
-//   };
-//   return urlsFiltered;
-// };
-
-// function userHasURL(userID, shortURL) {
-//   const userURLs = urlsForUser(userID);
-//   const userURLKeys = Object.keys(userURLs);
-//   return userURLKeys.includes(shortURL);
-// }
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
